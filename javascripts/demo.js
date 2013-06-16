cc855608f9038160727ccd6efdf4b7ce7b4aa73a
tim peterson <trpeters1@hotmail.com>
2013-06-16 13:13:41 -0400

    full demo

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,3 +1,22 @@
 
-var id='';
-var docViewer = new DocViewer({ id: "crocodoc"+id, zoom: "auto" });
+$('.showCrocodoc').click(function(){
+	var data=$(this).data();
+	$.ajax({
+		//"dataType": "json",
+		"url": 'https://onarbor.com/decode/getCrocodocSession/'+data.uuid,
+		success:function(data1){
+			console.log('1st callback'+data1);
+			$.ajax({
+			    url: "//crocodoc.com/webservice/document.js?session="+data1,
+			    dataType: "script",
+			    success: function(data2){
+			      	console.log('2nd callback'+data2);
+					var docViewer = new DocViewer({ id: "crocodoc-"+data.id, zoom: "auto" });	
+				}
+			});
+		}
+	});
+});
+
+
+
