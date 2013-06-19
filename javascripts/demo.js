a00c4c56724c2d89f665769b6ee2012f6ba9988f
tim peterson <trpeters1@hotmail.com>
2013-06-19 19:33:30 -0400

    added iosSlider

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,4 +1,6 @@
 
+
+
 $('.showCrocodoc').click(function(){
 	var data=$(this).data();
 	
@@ -15,7 +17,29 @@ $('.showCrocodoc').click(function(){
 			    dataType: "script",
 			    success: function(data2){
 			      	console.log('2nd callback'+data2);
-					var docViewer = new DocViewer({ id: "crocodoc-"+data.id, zoom: "auto" });	
+					var docViewer = new DocViewer({ id: "crocodoc-"+data.id, zoom: 0.5 });	
+
+			          docViewer.ready(function(e) {
+			          		var l=e.numpages;
+							console.log('docViewer ready numpages:'+l);
+
+								$('.iosSlider').iosSlider({
+									snapToChildren: true,
+									desktopClickDrag: true,
+									keyboardControls: true,
+									navNextSelector: $('.next'),
+									navPrevSelector: $('.prev'),
+									navSlideSelector: $('.selectors .item'),
+									onSlideChange: slideChange
+								});
+										
+								function slideChange(args) {							
+									$('.selectors .item').removeClass('selected');
+									$('.selectors .item:eq(' + (args.currentSlideNumber - 1) + ')').addClass('selected');								
+								}
+
+				          }); //docViewer.ready
+						
 				}
 			});
 		}
