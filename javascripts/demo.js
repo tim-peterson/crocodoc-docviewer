73087066e12154ec03c58f1d4c88653ca2c4845d
tim peterson <trpeters1@hotmail.com>
2013-06-19 19:48:10 -0400

    added all stuff from gh-pages to master

---
+++ javascripts/demo.js
@@ -0,0 +1,50 @@
+
+
+
+$('.showCrocodoc').click(function(){
+	var data=$(this).data();
+	
+	$(this).addClass('active');
+	$('.showCrocodoc').not(this).removeClass('active');
+
+	$.ajax({
+		//"dataType": "json",
+		"url": 'https://onarbor.com/decode/getCrocodocSession/'+data.uuid,
+		success:function(data1){
+			console.log('1st callback'+data1);
+			$.ajax({
+			    url: "//crocodoc.com/webservice/document.js?session="+data1,
+			    dataType: "script",
+			    success: function(data2){
+			      	console.log('2nd callback'+data2);
+					var docViewer = new DocViewer({ id: "crocodoc-"+data.id, zoom: 0.5 });	
+
+			          docViewer.ready(function(e) {
+			          		var l=e.numpages;
+							console.log('docViewer ready numpages:'+l);
+
+								$('.iosSlider').iosSlider({
+									snapToChildren: true,
+									desktopClickDrag: true,
+									keyboardControls: true,
+									navNextSelector: $('.next'),
+									navPrevSelector: $('.prev'),
+									navSlideSelector: $('.selectors .item'),
+									onSlideChange: slideChange
+								});
+										
+								function slideChange(args) {							
+									$('.selectors .item').removeClass('selected');
+									$('.selectors .item:eq(' + (args.currentSlideNumber - 1) + ')').addClass('selected');								
+								}
+
+				          }); //docViewer.ready
+						
+				}
+			});
+		}
+	});
+});
+
+
+
